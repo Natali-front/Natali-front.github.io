2173a854d94df4e71b2f15c5417682c329dfa129
Natali-front <78704034+Natali-front@users.noreply.github.com>
2021-02-24 02:20:06 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -5,7 +5,15 @@ $('.adv-type').slick({
     dots: true,
     arrows: true,
     
-})
-
-    
-   
+}); 
+// function initMap() {
+//         map = new google.maps.Map(document.getElementById('map'), {
+//             center: {lat: 40.668983, lng: -73.829057}
+//             zoom: 8,
+//         }),
+//         marker = new google.maps.Marker({
+//             position: {lat: 40.668983, lng: -73.829057},
+//             map: map,
+//             title: 'Mulitix big wordpress theme'
+//         });
+// }
